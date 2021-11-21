60eec2deb323e574bc7a2584d4eafa16735f01ae
Jesse Sites <jesse318s@gmail.com>
2021-11-21 15:00:07 -0600

    light mode

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
+import React, { useState } from 'react';
 import './App.scss';
 
 function App() {
+
+    const [isLightModeActive, setIsLightModeActive] = useState(false)
+
+    const switchModes = () => {
+        setIsLightModeActive(!isLightModeActive);
+    }
+
     return (
         <>
             <header>
                 {/* Navbar */}
-                <nav className="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+                <nav className={isLightModeActive ? "navbar navbar-expand-lg navbar-light bg-light fixed-top" : "navbar navbar-expand-lg navbar-dark bg-dark fixed-top"}>
                     {/* Container wrapper */}
                     <div className="container-fluid">
                         {/* Toggle button */}
@@ -25,7 +33,7 @@ function App() {
                         <div className="collapse navbar-collapse" id="navbarSupportedContent">
                             {/* Navbar brand */}
                             <a className="navbar-brand" href="">
-                                <h1 className="nav_title text_shadow">JS</h1>
+                                <h1 className="navbar_title text_shadow">JS</h1>
                             </a>
                             {/* Left links */}
                             <ul className="navbar-nav me-auto">
@@ -51,14 +59,14 @@ function App() {
 
                         {/* Right elements */}
                         <div className="d-flex align-items-center">
-                            <a className="btn btn-light me-4 shadow-4-strong" href="https://drive.google.com/file/d/1q_0IETZkUpfSghQxnjyHCSg_JvNHl68_/view?usp=sharing">
+                            <a className={isLightModeActive ? "toggle_light me-4 link-dark" : "toggle_light me-4 link-warning"} onClick={() => switchModes()}>
+                                <i className={isLightModeActive ? "fas fa-adjust fa-2x" : "fas fa-adjust fa-2x text_shadow"}></i>
+                            </a>
+                            <a className={isLightModeActive ? "btn btn-dark me-4 shadow-2-strong text-light" : "btn btn-light me-4 shadow-4-strong"} href="https://drive.google.com/file/d/1q_0IETZkUpfSghQxnjyHCSg_JvNHl68_/view?usp=sharing">
                                 My Resume
                             </a>
-                            <a className="me-4" href="https://www.linkedin.com/in/jesse-sites/">
-                                <i className="fab fa-linkedin-in fa-2x text_shadow"></i>
-                            </a>
                             <a className="me-4" href="https://github.com/jesse318s">
-                                <i className="fab fa-github fa-2x text_shadow"></i>
+                                <i className={isLightModeActive ? "fab fa-github fa-2x" : "fab fa-github fa-2x text_shadow"}></i>
                             </a>
                         </div>
                         {/* Right elements */}
@@ -68,8 +76,13 @@ function App() {
                 {/* Navbar */}
             </header>
 
-            <main className="bg-dark text-light">
-                <section style={{ background: `url('${process.env.PUBLIC_URL}/img/landing_img.webp')`, backgroundSize: "cover", height: "100vh", width: "99vw" }}>
+            <main className={isLightModeActive ? "bg-light text-dark" : "bg-dark text-light"}>
+                <section style={{
+                    background: `url('${process.env.PUBLIC_URL}/img/landing_img.webp')`,
+                    backgroundSize: "cover",
+                    height: "100vh",
+                    width: "99vw"
+                }}>
                     <p className="popout">
                         <span>J</span>
                         <span>E</span>
@@ -96,40 +109,40 @@ function App() {
                         <span>R</span>
                     </p>
                     <div className="d-flex justify-content-center">
-                        <a href="#skills" className="btn btn-dark btn-rounded btn-outline-* btn-lg blink">
+                        <a href="#skills"  className={isLightModeActive ? "btn btn-light btn-rounded btn-outline-* btn-lg blink" : "btn btn-dark btn-rounded btn-outline-* btn-lg blink"}>
                             <i className="fas fa-angle-down fa-2x"></i></a>
                     </div>
                 </section>
-                <div id="skills" style={{ position: "relative", bottom: "45px" }}></div>
+                <div id="skills" className="skills"></div>
                 <section className="row text-center mt-5 mx-5">
-                    <h2 className="mb-4 text_shadow">My Skills</h2>
+                    <h2 className={isLightModeActive ? "mb-4" : "mb-4 text_shadow"}>My Skills</h2>
                     <div className="col-6">
                         <p className="font-weight-bold pt-4 mb-2">HTML</p>
-                        <div className="progress" style={{ backgroundColor: "black" }}>
+                        <div className="progress bg-black">
                             <div className="progress-bar" role="progressbar" style={{ width: "100%", backgroundColor: "lightblue" }} aria-valuenow="25" aria-valuemin="0"
                                 aria-valuemax="100"></div>
                         </div>
                         <p className="mt-2">100%</p>
                         <p className="font-weight-bold pt-4 mb-2">CSS</p>
-                        <div className="progress" style={{ backgroundColor: "black" }}>
+                        <div className="progress bg-black">
                             <div className="progress-bar" role="progressbar" style={{ width: "98%", backgroundColor: "lightblue" }} aria-valuenow="25" aria-valuemin="0"
                                 aria-valuemax="100"></div>
                         </div>
                         <p className="mt-2">98%</p>
                         <p className="font-weight-bold pt-4 mb-2">SASS</p>
-                        <div className="progress" style={{ backgroundColor: "black" }}>
+                        <div className="progress bg-black">
                             <div className="progress-bar" role="progressbar" style={{ width: "95%", backgroundColor: "lightblue" }} aria-valuenow="25" aria-valuemin="0"
                                 aria-valuemax="100"></div>
                         </div>
                         <p className="mt-2">95%</p>
                         <p className="font-weight-bold pt-4 mb-2">JavaScript</p>
-                        <div className="progress" style={{ backgroundColor: "black" }}>
+                        <div className="progress bg-black">
                             <div className="progress-bar" role="progressbar" style={{ width: "95%", backgroundColor: "lightblue" }} aria-valuenow="25" aria-valuemin="0"
                                 aria-valuemax="100"></div>
                         </div>
                         <p className="mt-2">95%</p>
                         <p className="font-weight-bold pt-4 mb-2">Bootstrap</p>
-                        <div className="progress" style={{ backgroundColor: "black" }}>
+                        <div className="progress bg-black">
                             <div className="progress-bar" role="progressbar" style={{ width: "98%", backgroundColor: "lightblue" }} aria-valuenow="25" aria-valuemin="0"
                                 aria-valuemax="100"></div>
                         </div>
@@ -137,41 +150,41 @@ function App() {
                     </div>
                     <div className="col-6">
                         <p className="font-weight-bold pt-4 mb-2">React.js</p>
-                        <div className="progress" style={{ backgroundColor: "black" }}>
+                        <div className="progress bg-black">
                             <div className="progress-bar" role="progressbar" style={{ width: "95%", backgroundColor: "lightblue" }} aria-valuenow="25" aria-valuemin="0"
                                 aria-valuemax="100"></div>
                         </div>
                         <p className="mt-2">95%</p>
                         <p className="font-weight-bold pt-4 mb-2">Node.js</p>
-                        <div className="progress" style={{ backgroundColor: "black" }}>
+                        <div className="progress bg-black">
                             <div className="progress-bar" role="progressbar" style={{ width: "95%", backgroundColor: "lightblue" }} aria-valuenow="25" aria-valuemin="0"
                                 aria-valuemax="100"></div>
                         </div>
                         <p className="mt-2">95%</p>
                         <p className="font-weight-bold pt-4 mb-2">MongoDB</p>
-                        <div className="progress" style={{ backgroundColor: "black" }}>
+                        <div className="progress bg-black">
                             <div className="progress-bar" role="progressbar" style={{ width: "89%", backgroundColor: "lightblue" }} aria-valuenow="25" aria-valuemin="0"
                                 aria-valuemax="100"></div>
                         </div>
                         <p className="mt-2">89%</p>
                         <p className="font-weight-bold pt-4 mb-2">Express.js</p>
-                        <div className="progress" style={{ backgroundColor: "black" }}>
+                        <div className="progress bg-black">
                             <div className="progress-bar" role="progressbar" style={{ width: "89%", backgroundColor: "lightblue" }} aria-valuenow="25" aria-valuemin="0"
                                 aria-valuemax="100"></div>
                         </div>
                         <p className="mt-2">89%</p>
                         <p className="font-weight-bold pt-4 mb-2">GitHub</p>
-                        <div className="progress" style={{ backgroundColor: "black" }}>
+                        <div className="progress bg-black">
                             <div className="progress-bar" role="progressbar" style={{ width: "93%", backgroundColor: "lightblue" }} aria-valuenow="25" aria-valuemin="0"
                                 aria-valuemax="100"></div>
                         </div>
                         <p className="mt-2">93%</p>
                     </div>
-                    <hr class="my-5" />
+                    <hr className="my-5" />
                 </section>
-                <div id="projects" style={{ position: "relative", bottom: "95px" }}></div>
+                <div id="projects" className="projects"></div>
                 <section className="text-center mb-5">
-                    <h2 className="mb-5 text_shadow">My Projects</h2>
+                    <h2 className={isLightModeActive ? "mb-5" : "mb-5 text_shadow"}>My Projects</h2>
                     <div className="accordion mx-2" id="projectsAccordion">
                         <div className="accordion-item">
                             <h2 className="accordion-header" id="headingOne">
@@ -278,12 +291,12 @@ function App() {
                             </div>
                         </div>
                     </div>
-                    <hr class="my-5 mx-5" />
+                    <hr className="my-5 mx-5" />
                 </section>
-                <div id="about" style={{ position: "relative", bottom: "100px" }}></div>
+                <div id="about" className="about"></div>
                 <section className="text-center">
 
-                    <h2 className="mb-5 text_shadow">About Me</h2>
+                    <h2 className={isLightModeActive ? "mb-5" : "mb-5 text_shadow"}>About Me</h2>
 
                     <div className="row mx-5">
                         <div className="col-12">
@@ -294,17 +307,17 @@ function App() {
                                 my new place of study for Computer Programming.</p>
                             <p className="mb-0">I can create vanilla HTML/CSS/JS websites, React apps, and MERN stack apps.</p>
                         </div>
-                        <hr class="my-5" />
+                        <hr className="my-5" />
                     </div>
 
                 </section>
             </main>
 
-            <footer className="bg-dark text-light">
-                <div id="contact" style={{ position: "relative", bottom: "55px" }}></div>
+            <footer className={isLightModeActive ? "bg-light text-dark" : "bg-dark text-light"}>
+                <div id="contact" className="contact"></div>
                 <div className="text-center mb-5">
 
-                    <h2 className="mb-5 text_shadow">Contact Me</h2>
+                    <h2 className={isLightModeActive ? "mb-5" : "mb-5 text_shadow"}>Contact Me</h2>
 
                     <p className="mb-5 mx-auto w-responsive">
                         I can be contacted via LinkedIn, email, text, or call. Since a direct email may be caught in my spam filter,
@@ -313,23 +326,23 @@ function App() {
 
                     <ul className="list-unstyled mb-0">
                         <li>
-                            <i className="fab fa-linkedin-in fa-2x text-light text_shadow"></i>
+                            <i className={isLightModeActive ? "fab fa-linkedin-in fa-2x text-dark" : "fab fa-linkedin-in fa-2x text-light text_shadow"}></i>
                             <p className="text-decoration-underline">
                                 <a href="https://www.linkedin.com/in/jesse-sites/">https://www.linkedin.com/in/jesse-sites/</a>
                             </p>
                         </li>
                         <li>
-                            <i className="fas fa-envelope fa-2x text-light text_shadow"></i>
+                            <i className={isLightModeActive ? "fas fa-envelope fa-2x text-dark" : "fab fa-linkedin-in fa-2x text-light text_shadow"}></i>
                             <p>jesse318s@gmail.com</p>
                         </li>
                         <li>
-                            <i className="fas fa-phone fa-2x text-light text_shadow"></i>
+                            <i className={isLightModeActive ? "fas fa-phone fa-2x text-dark" : "fab fa-linkedin-in fa-2x text-light text_shadow"}></i>
                             <p>+1 325-455-4644</p>
                         </li>
                     </ul>
 
                 </div>
-                <div className="text-center p-3" style={{ backgroundColor: "rgba(0, 0, 0, 0.2)" }}>
+                <div className="copyright text-center p-3">
                     © 2021 Copyright: Jesse Sites
                 </div>
             </footer>
